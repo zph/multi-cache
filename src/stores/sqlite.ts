b629d01c746698e966c76bb44a5895ddd7e8e3c7
Zander Hill <zander@xargs.io>
2024-06-25 00:28:10 -0700

    Fix more jsr

--- src/stores/sqlite.ts
+++ src/stores/sqlite.ts
@@ -2,11 +2,11 @@
  * @resolid/cache-manager-sqlite
  * const sqliteStoreCache = cacheManager.caching(sqliteStore({sqliteFile: join(process.cwd(), 'cache.sqlite3'), cacheTableName: 'caches'}))
  */
-import { sqliteStore } from 'npm:@resolid/cache-manager-sqlite@5.1.5'
+import { SqliteStore, sqliteStore } from 'npm:@resolid/cache-manager-sqlite@5.1.5'
 import * as path from "jsr:@std/path@1.0.0-rc.2";
 
 const sqliteFile = path.join(Deno.cwd(), '/cache.sqlite3')
 const cacheTableName = 'caches'
-export const defaultSqliteStore = sqliteStore({sqliteFile, cacheTableName})
+export const defaultSqliteStore: SqliteStore = sqliteStore({sqliteFile, cacheTableName})
 
 export { sqliteStore } from 'npm:@resolid/cache-manager-sqlite@5.1.5'
